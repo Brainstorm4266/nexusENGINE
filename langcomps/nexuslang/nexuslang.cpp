30bdd941416d00f2cba97b77f75889b9a174fbf9
Andrei Dmitriev <dmitriev.ps3@outlook.com>
2022-03-23 22:13:05 +0100

    Updated nexuslang to add eval feature

--- langcomps/nexuslang/nexuslang.cpp
+++ langcomps/nexuslang/nexuslang.cpp
@@ -14,9 +14,23 @@
 using namespace std;
 using namespace Tokenizer;
 
-int main() {
+int main(int argc, char *argv[]) {
     //function{\n  for i,v in getfenv() {\n        print(i,v)\n    }\n}
-    string text = "5*9"; // FIX TOKENIZER ISSUE!
+    string text = "5*9"; // FIX TOKENIZER ISSUE!     me from later: what was this issue again?
+    // accept argument -e to enter text from stdin
+    if (argc > 1 && string(argv[1]) == "-e") {
+        stringstream ss;
+        string line;
+        while (getline(cin, line)) {
+            if (line == "<EOF>") {
+                // remove last newline
+                text = text.substr(0, text.size() - 1);
+                break;
+            }
+            ss << line << endl;
+        }
+        text = ss.str();
+    }
     std::cout << "nexuslang v0.0.0\nCopyright 2022 Andrei Dmitriev\n\n[TKN]: Tokenizing code located in binary... ";
     char* text_char = const_cast<char*>(text.c_str());
     vector<pair<string, int>> LEXED_CONTENT = tokenize(text_char);
