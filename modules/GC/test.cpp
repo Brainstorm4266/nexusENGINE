680ae9341fada1179e101115be2bcc3cf1c8902d
BuildTools <unconfigured@null.spigotmc.org>
2022-04-22 01:58:57 +0300

    e

--- modules/GC/test.cpp
+++ modules/GC/test.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 int main() 
 {
-    using namespace gc;
+    using namespace gc_old;
     Object<Object<int>> o2 = Object<int>(52);
     std::cout<< o2.getrefCnt() << std::endl;
     Object<Object<int>> o1 = o2;
